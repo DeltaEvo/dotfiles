0bbecde1b185709fe90a94c456630d46871387e7
Duarte David <deltaduartedavid@gmail.com>
2016-11-12 22:57:35 +0100

    Add dotfiles

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+# Path to Oh My Fish install.
+set -q XDG_DATA_HOME
+  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+  or set -gx OMF_PATH "$HOME/.local/share/omf"
+
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG "/home/david/.config/omf"
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
