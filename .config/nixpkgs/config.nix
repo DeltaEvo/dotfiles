6ab12ab4b0888bd19f68c441b6b75e8921c344d9
Duarte David <deltaduartedavid@gmail.com>
2017-11-11 13:02:06 +0100

    Update albert

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -3,6 +3,7 @@
   steam.primus = true;
   packageOverrides = pkgs: rec {
     home-manager = import ./home-manager { inherit pkgs; };
+    python3 = pkgs.python3.withPackages (ps : [ps.numpy]);
   };
   # chromium = { enablePepperFlash = true; };
 }
